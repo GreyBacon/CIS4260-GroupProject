41c389cfa0c9298396cccca96522cc500188f6ed
Ken Wood <42664574+GreyBacon@users.noreply.github.com>
2019-03-01 18:04:35 -0800

    Basic page formatting and css to work with

--- index.php
+++ index.php
@@ -4,10 +4,14 @@ require_once('database.php');
 ?>
 <html>
     <head>
-        <meta charset="UTF-8">
+        <meta charset="UTF-8"> 
         <title></title>
+        <link rel="stylesheet" type="text/css" href="main.css" />
     </head>
-    <body>
-        <h>test</h>  
+    <body class="flexbox-container">
+    <div class="mainBody">
+        
+        <h>test</h>   
+    </div>
     </body>
 </html>
