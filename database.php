138bffd268d5c4deebbc766e6384e252e4096d71
bryanliu76 <42634462+bryanliu76@users.noreply.github.com>
2019-03-02 17:01:20 -0800

    create new cartIndex

--- database.php
+++ database.php
@@ -3,6 +3,9 @@ $servername = 'mysql:host=localhost;dbname=groupproject_db';
 $username = 'root';
 $password = '';
 
+$conn=mysql_connect($servername, $username, $password);
+mysql_select_db($mysql_database, $conn);
+mysql_query("set names 'utf8'");
 try {
     $db = new PDO($servername, $username, $password);
 } catch (PDOException $e) {
