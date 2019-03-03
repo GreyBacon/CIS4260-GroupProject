f7adac82b4f412c86968ec10bf70258c21a7b9db
Ken Wood <42664574+GreyBacon@users.noreply.github.com>
2019-03-02 22:18:03 -0800

    Detail page filled out
    Probably ready for shopping cart functions

--- detail.php
+++ detail.php
@@ -3,4 +3,9 @@
 
     $game = get_detail($game_id);
 ?>
+<img src="images\<?php echo $game['image'];?>"/>
 <h1><?php echo $game['game']; ?></h1>
+<h5>Release date: <?php echo $game['releaseDate']; ?></h5>
+<h2>$<?php echo $game['price']; ?></h2>
+
+<!-- Probably a good place to include add to cart-->
